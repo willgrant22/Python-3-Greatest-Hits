16a7f677b3decc31bbb48e999364115d8d588a29
willgrant22 <will.grant.22@protonmail.com>
2020-08-01 20:09:09 -0400

    add
    added files

---
+++ tkinter/tk_jumping_btn.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# =============================================================================
+# Author :  Will Grant
+# =============================================================================
+
+import tkinter
+import random
+
+def onEnterButton(e):
+
+    w = root.winfo_width()
+    h = root.winfo_height()
+
+    b_w = btn.winfo_width()
+    b_h = btn.winfo_height()
+
+    r_x = random.randrange(0, w - b_w)
+    r_y = random.randrange(0, h - b_h)
+
+    btn.place(x=r_x, y=r_y)
+
+
+root = tkinter.Tk()
+root.title('Jumping button')
+root.resizable(False, False)
+
+btn = tkinter.Button(root, text='Exit', width=10, command=root.quit)
+btn.place(x=180, y=20)
+
+btn.bind('<Enter>', onEnterButton)
+
+root.geometry("600x600+30+30")
+root.mainloop()
