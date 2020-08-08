d3007238ccb64e01a87f9bce88b9023de8e5025f
willgrant22 <will.grant.22@protonmail.com>
2020-08-08 15:26:42 -0400

    Name changes, restructuring, added scripts.

---
+++ Barcode/qr_svg_scale.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# =============================================================================
+# Author :  Will Grant
+# =============================================================================
+import pyqrcode 
+
+# Initialize qr data 
+s = "test"
+
+# Generate QR code by the help of create function 
+url = pyqrcode.create(s) 
+
+# saving the svg file naming "qr.svg" 
+url.svg("myqr.svg", scale = 5)
+
+#1 = 29px 29px
+#2 = 58px 58px
+#3 = 87px 87px
