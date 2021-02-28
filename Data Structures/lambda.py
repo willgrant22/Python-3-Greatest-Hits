8ab30cf8747d3e512ec824f51731c6ecaf234651
Will Grant <will@CAEIP4C.home>
2021-02-28 15:59:44 -0500

    Removed redundant files // Combined like files

--- Data Structures/lambda.py
+++ Data Structures/lambda.py
@@ -6,3 +6,22 @@
 
 adder = lambda x, y: x + y
 print (adder (1, 2))
+
+double = lambda x: x * 2
+
+# Output: 10
+print(double(5))
+
+my_list1 = [1, 5, 4, 6, 8, 11, 3, 12]
+
+new_list1 = list(filter(lambda x: (x%2 == 0) , my_list1))
+
+# Output: [4, 6, 8, 12]
+print(new_list1)
+
+my_list2 = [1, 5, 4, 6, 8, 11, 3, 12]
+
+new_list2 = list(map(lambda x: x * 2 , my_list2))
+
+# Output: [2, 10, 8, 12, 16, 22, 6, 24]
+print(new_list2)
